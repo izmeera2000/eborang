7a0ab2f4bec002344d9680895ade1ac838c2b740
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2025-05-07 15:24:30 +0800

    asd
    asdsa

--- includes/server.php
+++ includes/server.php
@@ -7,35 +7,7 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
 $dotenv->load();
 
  
-use Pusher\PushNotifications\PushNotifications;
-
-
-
-function publishToBeamsInterests(array $interests, string $title, string $body, string $deepLink): ?array
-{
-
-    $beamsClient = new PushNotifications([
-        "instanceId" => $_ENV['pusher_9_id'],
-        "secretKey" => $_ENV['pusher_9_key']
-    ]);
-
-    $beamsClient->publishToInterests(
-        $interests,
-        [
-            'web' => [
-                'notification' => [
-                    'title' => $title,
-                    'body' => $body,
-                    'deep_link' => $deepLink,
-                ],
-            ],
-        ]
-    );
-
-
-}
-
-
+ 
 
 
 require('database.php');
